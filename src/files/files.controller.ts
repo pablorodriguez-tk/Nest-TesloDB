eae67a9ec721617ca932cefc9d49ee8779c9ca99
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-03-29 23:43:27 -0300

    env for path

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,13 +3,13 @@ import {
   Controller,
   Get,
   Post,
-  Res,
   UploadedFile,
   UseInterceptors,
   Param,
   StreamableFile,
   Header,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createReadStream } from 'fs';
 import { diskStorage } from 'multer';
@@ -18,7 +18,10 @@ import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('product')
   @UseInterceptors(
@@ -38,15 +41,17 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
 
     return {
-      fileName: secureUrl,
+      secureUrl,
     };
   }
 
   @Get('product/:imageName')
-  @Header('Content-Type', 'image/jpeg')
+  @Header('Content-Type', 'image/png')
   findProductImage(@Param('imageName') imageName: string) {
     const stream = createReadStream(
       this.filesService.getStaticProductImage(imageName),
