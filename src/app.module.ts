cdbeffcdeebfffa80e78e9dd320de6041cb0b5d4
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-04-01 17:23:21 -0300

    initial config websockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { FilesModule } from './files/files.module';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import { AuthModule } from './auth/auth.module';
     SeedModule,
     FilesModule,
     AuthModule,
+    MessagesWsModule,
   ],
 })
 export class AppModule {}
