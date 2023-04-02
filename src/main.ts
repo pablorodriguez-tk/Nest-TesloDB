c8fd054e5eb721f25b2e1fa86699ab366ad9ad11
Pablo Rodriguez <pablorodriguez88@gmail.com>
2023-04-02 18:45:28 -0300

    cors problem test

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN,
-  });
+  app.enableCors();
   const logger = new Logger('Bootstrap');
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
